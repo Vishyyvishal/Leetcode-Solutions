f284d4c7e04c6f4c26c52581e1f4645d1ff87dd5
Vishal Rachapudi <56774642+Vishyyvishal@users.noreply.github.com>
2022-06-11 14:00:05 -0500

    Time: 12 ms (31.92%), Space: 46.5 MB (35.54%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    /*
+        1. create a map with roman chars to integer
+        2. scan the string from right to left. get the integer value of the current 
+           character from the map and add it to the result
+        3. if the character tot he right of the current character is bigger than the current
+           character, subtrat from the result
+           
+    
+    */
+    
+    public int romanToInt(String s) {
+        // map: roman char -> integer value
+        Map<Character, Integer> map = new HashMap<>();
+        
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        char lastChar = s.charAt(s.length() - 1);
+        int res = map.get(lastChar);
+        
+        // scan the string from right to left 
+        // start at 2nd to last char so we can look at 2 chars at a time
+        for(int i = s.length() - 2; i >= 0; i--) {
+            char currChar = s.charAt(i);
+            char nextChar = s.charAt(i + 1);
+            
+            if(map.get(currChar) >= map.get(nextChar)) {
+                res += map.get(currChar);
+            } else {
+                // if char at the right is bigger -> special rule: subtract
+                res -= map.get(currChar);
+            }
+        }
+        return res;
+    }
+}

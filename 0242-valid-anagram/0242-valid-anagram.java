ff9611a148a33aef4a8f8751d8c5b3fca88f2f5e
Vishal Rachapudi <56774642+Vishyyvishal@users.noreply.github.com>
2023-01-09 16:26:07 -0600

    Time: 4 ms (74.34%), Space: 42.4 MB (82.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // base case 1 
         if(s.length() != t.length()) return false;
-        // base case 2 
-        if(s.equals(t)) return true;
-
-        Map<Character, Integer> map = new HashMap<>();
         
-        for(char ch : s.toCharArray()) {
-            if(map.containsKey(ch)) {
-                int occurence = map.get(ch) + 1;
-                map.put(ch, occurence);
-            } else {
-                map.put(ch, 1);
-            }
+        int[] store = new int[26];
+        
+        for(int i = 0; i < s.length(); i++) {
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
         }
         
-        for(char ch : t.toCharArray()) {
-            if(map.containsKey(ch)) {
-                int sub = map.get(ch) - 1;
-                map.put(ch, sub);
-                if(map.get(ch) == 0) map.remove(ch);
-            }
-        }
+        for(int num : store) if(num != 0) return false;
         
-        return map.isEmpty(); 
+        return true;
     }
 }

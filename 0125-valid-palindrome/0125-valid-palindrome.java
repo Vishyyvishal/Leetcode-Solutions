ff4155be6a134a66e942d53a71091302d262d6bc
Vishal Rachapudi <56774642+Vishyyvishal@users.noreply.github.com>
2023-01-11 15:53:29 -0600

    Time: 733 ms (23.63%), Space: 43.4 MB (57.90%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
         int front = 0; 
         int back = s.length() - 1;
         

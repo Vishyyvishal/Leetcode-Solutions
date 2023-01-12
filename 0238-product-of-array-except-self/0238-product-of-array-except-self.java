1632ff25b0cb3bdeb35c9ea3bfa522b4244667e6
Vishal Rachapudi <56774642+Vishyyvishal@users.noreply.github.com>
2023-01-12 15:06:56 -0600

    Time: 2 ms (68.19%), Space: 50.6 MB (77.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,24 +1,50 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] pre = new int[n];
-        int[] post = new int[n];
-        pre[0] = 1;
-        post[n - 1] = 1;
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
         
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
-        }
-        
-        for(int i = n - 2; i >= 0; i--) {
-            post[i] = post[i + 1] * nums[i + 1];
-        }
-        
-        int res[] = new int[n];
+        // prefix portion
+        int curr = 1;
         for(int i = 0; i < n; i++) {
-            res[i] = pre[i] * post[i];
+            res[i] *= curr;
+            curr *= nums[i];
+        }
+        
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            res[i] *= curr;
+            curr *= nums[i];
         }
         
         return res;
     }
 }
+
+
+/*
+Time: O(N) Space: O(N)
+Not the most efficient
+
+    int n = nums.length;
+    int[] pre = new int[n];
+    int[] post = new int[n];
+    pre[0] = 1;
+    post[n - 1] = 1;
+
+    for(int i = 1; i < n; i++) {
+        pre[i] = pre[i - 1] * nums[i - 1];
+    }
+
+    for(int i = n - 2; i >= 0; i--) {
+        post[i] = post[i + 1] * nums[i + 1];
+    }
+
+    int res[] = new int[n];
+    for(int i = 0; i < n; i++) {
+        res[i] = pre[i] * post[i];
+    }
+
+    return res;
+
+*/
